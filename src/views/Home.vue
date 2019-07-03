34240526331a91f476f7137e1c5cda21c149e066
lojze <314117032@qq.com>
2019-07-03 17:55:09 +0800

    set loading

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <button>防抖测试</button>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+export default class Home extends Vue {
+  // messgae = '防抖测试';
+  // timer = 0;
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
+  // 立即执行
+  // public debounce (func:Function, delay:number) {
+  //   // const timeout:any;
+  // }
+}
 </script>
